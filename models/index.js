9aa9c0d1e8b45e2500d680f68e4c2eb6d9556b63
Lawrence Fiesta <lfiesta86@gmail.com>
2019-07-16 13:12:13 -0700

    models update

--- models/index.js
+++ models/index.js
@@ -27,7 +27,8 @@ fs.readdirSync(__dirname)
   })
   .forEach(function(file) {
     var model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
+    // console.log(file);
+    // db[model.name] = model;
   });
 
 Object.keys(db).forEach(function(modelName) {
