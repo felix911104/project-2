ad090d6f5482c380a030f02ae60530c3c80fe4b6
Felix <46941065+felix911104@users.noreply.github.com>
2019-07-16 13:31:10 -0700

    survey first upload

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -12,12 +12,9 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/survey", function(req, res) {
+    
+      res.render("survey");
   });
 
   // Render 404 page for any unmatched routes
