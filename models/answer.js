b74523336af7e0638787d8594906a1136139a1ef
Lawrence Fiesta <lfiesta86@gmail.com>
2019-07-17 21:42:58 -0700

    fixed a few issues after the huge front end pull

---
+++ models/answer.js
@@ -0,0 +1,53 @@
+module.exports = (sequelize, DataTypes) => {
+  var Answer = sequelize.define("Answer", {
+    q1: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    q2: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    q3: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    q4: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    q5: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    q6: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    q7: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    q8: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    q9: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    q10: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  Answer.associate = models => {
+    Answer.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  }
+  return Answer;
+} 
